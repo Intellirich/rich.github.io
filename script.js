2d999477ca3abdeb0c18bdffda90d79ab03a4ffe
Intellirich <armellini.r@gmail.com>
2016-11-17 14:25:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ var map = L.map('mapid').setView([45.4632, 9.1886], 12);
 				$.ajax({
 				url: URL,
 				dataType: 'jsonp',
-				success: loadGeoJson
+				success: loadGeoJson()
 			});
 				
 			};
