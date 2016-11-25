5d9dc65ae639fc9a598c2cfa09eb2fcdd15e3af3
Intellirich <armellini.r@gmail.com>
2016-11-25 10:11:52 +0100

    max feature 500

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var map = L.map('mapid').setView([45.4632, 9.1886], 12);
 
 		          typeName: 'BIopen:grandi_strutt_vendita',
 
-		          maxFeatures: '100',
+		          maxFeatures: '500',
 
 		          outputFormat: 'text/javascript',
 
